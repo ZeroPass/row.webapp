02fb6fdfa33a23cddc47c019a48ba139a7ba8902
Nejc SKerjanc <nejko90@gmail.com>
2021-04-05 19:10:40 +0000

    port changed

--- src/server/server-loader.js
+++ src/server/server-loader.js
@@ -48,7 +48,7 @@ io.on('connection', socket => {
     getServerMain().connected(socket);
 });
 
-serverhttps.listen(8000);
+serverhttps.listen(443);
 
 const formatHost = {
     getCanonicalFileName: path => path,
