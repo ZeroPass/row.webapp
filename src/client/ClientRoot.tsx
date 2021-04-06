f6a82624770724309817df0e62e4044c9ff9e317
Nejc SKerjanc <nejko90@gmail.com>
2021-04-06 06:52:25 +0000

    cliend side is clean

--- src/client/ClientRoot.tsx
+++ src/client/ClientRoot.tsx
@@ -77,18 +77,9 @@ export class AppState {
   //  this.proposalName = prososalName;
   //}
 
-  public restore(prev: AppState) {
-    /*
-        this.message = prev.message;
-        this.setKeys(prev.keys);*/
-  }
+  public restore(prev: AppState) {}
 
-  public setKeys(keys: Key[]) {
-    /*this.keys = keys;
-        this.sigprov.keys.clear();
-        for (const key of this.keys)
-            this.sigprov.keys.set(key.key, key.credentialId);*/
-  }
+  public setKeys(keys: Key[]) {}
 }
 
 function appendMessage(appState: AppState, message: string) {
@@ -111,7 +102,6 @@ function connectSocket(appState: AppState) {
     appState.clientRoot.forceUpdate();
   });
 }
-//////
 
 interface AddKeyArgs {
   rpid: string;
@@ -615,7 +605,6 @@ export async function propose(appState: AppState) {
     let seActions = await appState.connector.serializeTransaction( TEMP_RAW_TRANSACTION );
     console.log(seActions[0].data);
 
-    //"2021-03-18T11:25:23",
     var TEMP_FIXED_TRANSACTION = {
       expiration: moment(timestamp).format("YYYY-MM-DDTHH:mm:ss"),
       ref_block_num: 0,
@@ -684,36 +673,6 @@ export async function propose(appState: AppState) {
     //show on UI
     appendMessage(appState, e);
   }
-}
-
-
-
-function getLastProposal(proposals: any[]): ProposalStruct {
-  if (proposals.length == 0)
-    throw new Error(
-      "No proposals under current account. Please add proposal first to your account to use current action."
-    );
-
-  var last = proposals.length - 1;
-  var proposal_name = proposals[last].proposal_name;
-  var data = proposals[last].packed_transaction;
-
-  console.log("Last proposal; proposal name:" + proposal_name + ", data: " + data);
-  return new ProposalStruct(proposal_name, Serialize.hexToUint8Array(data));
-}
-
-function getProposalTx(proposals: any[]): ProposalStruct {
-  if (proposals.length == 0)
-    throw new Error(
-      "No proposals under current account. Please add proposal first to your account to use current action."
-    );
-
-  var last = proposals.length - 1;
-  var proposal_name = proposals[last].proposal_name;
-  var data = proposals[last].packed_transaction;
-
-  console.log("Last proposal; proposal name:" + proposal_name + ", data: " + data);
-  return new ProposalStruct(proposal_name, Serialize.hexToUint8Array(data));
 }
 
 export async function approve(appState: AppState): Promise<boolean> {
