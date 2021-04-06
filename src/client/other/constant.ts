b837dca23edd599c315cb273b90199919aff562c
Nejc <nejc.skerjanc01@gmail.com>
2021-04-06 20:33:28 +0000

    final updates

--- src/client/other/constant.ts
+++ src/client/other/constant.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     eosio: {
         //host: 'https://jungle3.greymass.com',
-        host: 'https://163.172.144.187:9899', //'http://localhost:9898',
+        host: 'https://ubi.world:9899', //'http://localhost:9898',
         contract: 'irowyourboat',
         privateKeyTemp: '5JwiahEGHWPjs2rgqgTUffgEKyYMpuudFtvZEfsJtYNEx26q98Z',
         blockExplorerURL: 'https://local.bloks.io/account/eosio?nodeUrl=https%3A%2F%2F163.172.144.187%3A9899'
