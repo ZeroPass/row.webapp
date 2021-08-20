42b30e9a1dd28b8a3313572a310a16903f36c94a
smlu <smlu@s5.net>
2021-08-20 02:53:28 +0200

    Fix trx permission for `addkey` action

--- src/client/connector/connector.ts
+++ src/client/connector/connector.ts
@@ -159,7 +159,7 @@ export class Connector{
                         },
                         authorization: [{
                             actor: user,
-                            permission: 'rowmsig',
+                            permission: 'active',
                         }],
                     }],
                 }, {
